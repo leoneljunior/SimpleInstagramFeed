50e2826a9279b336476a71e5cc8529bd472855eb
leoneljunior <LeonelJR@MacBook-Pro-de-Raimundo.local>
2016-05-07 01:14:36 -0300

    [Leonel] searching by hashtag.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,21 @@
 angular.module('starter.controllers', [])
 
 .controller('FeedCtrl', function($scope, $http) {
-    var BASE_URL = "https://api.instagram.com/v1/tags/programming/media/recent?access_token=3210548664.5468d74.9a036c53afd640e1a417aa44b815bcff&callback=JSON_CALLBACK";
+    var BASE_URL_1 = "https://api.instagram.com/v1/tags/";
+    var BASE_URL_2 = "/media/recent?access_token=3210548664.5468d74.9a036c53afd640e1a417aa44b815bcff&callback=JSON_CALLBACK";
+    $scope.hashtag = {name:"amor"};
+    $scope.data = [];
     $scope.getMedia = function(){
-    $http.jsonp(BASE_URL).then(function(response) {
-      data = response.data.data;
-      //nextUrl = response.data.pagination.next_max_tag_id;
-      //NewInsta = response.data.pagination.min_tag_id;
-      //console.log(nextUrl, NewInsta);
-      console.log(data);
-      $scope.data = data;
-    });
-  };
+      $http.jsonp(BASE_URL_1 + $scope.hashtag.name + BASE_URL_2).then(function(response) {
+        data = response.data.data;
+        //nextUrl = response.data.pagination.next_max_tag_id;
+        //NewInsta = response.data.pagination.min_tag_id;
+        //console.log(nextUrl, NewInsta);
+        console.log(data);
+        console.log($scope.hashtag);
+        $scope.data = data;
+      });
+    };
 
   $scope.getMedia();
 })
